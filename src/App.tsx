8bc070e3456c58556c2e40161a54804aacc32529
Wellington R <sousa_wellington@outlook.com>
2021-05-21 15:55:08 -0300

    Fix: 🐛Security CORS, bacause two hosts

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,25 @@ import { useEffect } from "react";
 import { Header } from "./components/Header";
 import { Nav } from "./components/Nav";
 import { Section } from "./components/Section";
+import { api } from "./Services";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
 
+  // useEffect(()=>{
+  //   fetch('/api').then(response => {
+  //     if(response.ok){
+  //       return response.json();
+  //     }
+  //   }).then(data=> console.log(data)); 
+  // },[])
+
+
   useEffect(()=>{
-    fetch('/api').then(response => {
-      if(response.ok){
-        return response.json();
-      }
-    }).then(data=> console.log(data)); 
+    api.get('/api')
+    .then(response=> console.log(response.data)); 
   },[])
+
   return (
     <>
       <GlobalStyle />
