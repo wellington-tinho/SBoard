a2b9fbdc38fe9140c5039d41a8c3c5fa8bdf609d
Wellington R <sousa_wellington@outlook.com>
2021-05-25 23:46:26 -0300

    Att: up new pakage (sigma)

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,6 @@ import { api } from "./Services";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
-
-  // useEffect(()=>{
-  //   fetch('/api').then(response => {
-  //     if(response.ok){
-  //       return response.json();
-  //     }
-  //   }).then(data=> console.log(data)); 
-  // },[])
-
-
   useEffect(()=>{
     api.get('/api')
     .then(response=> console.log(response.data)); 
