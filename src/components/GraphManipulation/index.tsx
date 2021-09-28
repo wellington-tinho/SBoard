239d18684c69ef4a63ee948f5accfb9cc60a80cb
Wellington R <sousa_wellington@outlook.com>
2021-09-27 22:26:36 -0300

    ✨ add: tap on data in edge and nodes,

--- src/components/GraphManipulation/index.tsx
+++ src/components/GraphManipulation/index.tsx
@@ -15,7 +15,7 @@ export interface iGraphJson{
 
 export function GraphManipulation({grapJSON}:propsGraphJson){
   const containerRef = useRef(null);
-  const setCy = useContext(CytoscapeContext)[1]
+  const [cy,setCy] = useContext(CytoscapeContext)
 
   Object.keys(grapJSON.edges)
     .forEach(key=>{
@@ -45,43 +45,74 @@ export function GraphManipulation({grapJSON}:propsGraphJson){
   const elementos = CytoscapeComponent.normalizeElements({nodes: grapJSON.nodes, edges: grapJSON.edges});
   
   
- 
+//  // funções automaticas inicializadas junto com o grafico 
+// function CytoscapeFunctions(){
+//       try {
+//         cy.on('tap', 'node', function(evt:any){
+//           var node = evt.target;
+//           console.log( 'tapped ' + node.id() );
+//         });   
+//      } catch (error) { console.log('CytoscapeFunctions',error)}
+// } 
+// CytoscapeFunctions()
   
  
   
 
 
   useEffect(() => {
-  const config = {
-    container: containerRef.current,
-      layout:{
-        name: "breadthfirst",
-        fit: true,
-        // circle: true,
-        directed: true,
-        padding: 50,
-        // spacingFactor: 1.5,
-        animate: true,
-        animationDuration: 1000,
-        avoidOverlap: true,
-        nodeDimensionsIncludeLabels: false
-      },
-      style: [
+    const config = {
+      container: containerRef.current,
+        layout:{
+          name: "breadthfirst",
+          fit: true,
+          // circle: true,
+          directed: true,
+          padding: 50,
+          // spacingFactor: 1.5,
+          animate: true,
+          animationDuration: 1000,
+          avoidOverlap: true,
+          nodeDimensionsIncludeLabels: false
+        },
+        style: [
+        
+          {selector: "node",
+          style: { content: "data(label)" }},
       
-        {selector: "node",
-        style: { content: "data(label)" }},
-    
-        // {selector: 'edge',
-        // class: {content:"data(id)"}},
-      ],
-      elements:elementos,
-      minZoom: 0.1,
-      maxZoom:3
-    };
-      cytoscape(config);
+          // {selector: 'edge',
+          // class: {content:"data(id)"}},
+        ],
+        elements:elementos,
+        minZoom: 0.1,
+        maxZoom:3
+      };
+      // cytoscape(config);
       setCy(cytoscape(config))
-      
     }, []);
+
+    // funções automaticas inicializadas junto com o grafico 
+    useEffect(() => {
+      function CytoscapeFunctions(){
+            try {
+              cy.on('tap', function(event:any){
+                if( event.target === cy ){
+                  console.log('tap on background');
+                } 
+              })
+              
+              cy.on('tap', 'node', function(evt:any){
+                console.log( 'tapped ', (evt.target).data() )
+              })
+
+              cy.on('tap', 'edge', function(evt:any){
+                console.log( 'tapped ', (evt.target).data() )
+              });
+          } catch (error) { console.log('CytoscapeFunctions',error)}
+      } 
+      CytoscapeFunctions()
+  
+    },[cy])
   
  
 
