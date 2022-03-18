12d2105265e2aca7662a8c476d60ff9410183803
Wellington R <sousa_wellington@outlook.com>
2022-03-18 15:31:51 -0300

    🔧config process.env for connect api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,16 @@
 import axios from 'axios';
+ 
+var API
+if (process.env.NODE_ENV !== 'production') {
+   API = ('http://127.0.0.1:5000/')
+  
+} else {
+   API = ('https://sboard-api.herokuapp.com/')
+  
+}
 
 export const api = axios.create({
-  baseURL: 'http://127.0.0.1:5000/',
+  baseURL: API,
+  
 })
+
