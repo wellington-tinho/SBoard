508e2113b251a43ca3301de6041f86ae84845fef
Wellington R <sousa_wellington@outlook.com>
2022-07-01 21:44:45 -0300

    🐛 fix: label

--- src/screen/home/Section/GraphArea/index.tsx
+++ src/screen/home/Section/GraphArea/index.tsx
@@ -2,7 +2,7 @@ import { lazy, Suspense, useContext, useEffect, useRef, useState } from 'react';
 import { CytoscapeContext } from '../../../../CytoscapeContext';
 import { StartGraph } from '../../../../components/startGraph';
 import useInitCytoscapeExtensions from '../../../../hooks/useInitCytoscapeExtensions';
-const SetupModal = lazy((): Promise<any> =>  import('../../../../components/SetupModal'));
+const SetupModal = lazy((): Promise<any> => import('../../../../components/SetupModal'));
 
 import { AiOutlineZoomIn, AiOutlineZoomOut, AiOutlineExpandAlt, AiOutlineShrink } from 'react-icons/ai';
 import { BiGitPullRequest } from 'react-icons/bi';
@@ -22,15 +22,15 @@ import { Container, GraphContainer, NavOptions } from "./styles";
 // import { SetupModal } from '../SetupModal';
 
 
-  
+
 
 Modal.setAppElement('#root')
 
-export function GraphArea({setRequest}:any){
+export function GraphArea({ setRequest }: any) {
   const [cy] = useContext<cytoscape.Core[]>(CytoscapeContext);
   const [element, setElement] = useState({} as any)
   const [isSetupModal, setIsSetupModal] = useState(false);
-  const hiddenFileRequestInput = useRef<any>(null); 
+  const hiddenFileRequestInput = useRef<any>(null);
   // const [eh, setEh] = useState(undefined as unknown as EdgeHandlesInstance);
   const [drawMode, setDrawMode] = useState(false);
   const layouts = [
@@ -50,36 +50,36 @@ export function GraphArea({setRequest}:any){
       fit: true
     },
     {
-      name:'circle',
+      name: 'circle',
       fit: true
     },
     {
-      name:'random',
+      name: 'random',
       fit: true
     },
     {
-      name:'cose',
+      name: 'cose',
       fit: true
     },
     {
-      name:'grid',
+      name: 'grid',
       fit: true
     },
     {
-      name:'breadthfirst',
+      name: 'breadthfirst',
       fit: true
-  }]
+    }]
   const { edgeHandles } = useInitCytoscapeExtensions(cy);
   var count = 0
 
 
-  useEffect( () => {
-    if(cy){
+  useEffect(() => {
+    if (cy) {
       cy?.on('tap', (event: any) => {
         setElement(event.target._private.data)
       });
-     }
-  },[cy])
+    }
+  }, [cy])
 
   // Update edgehandles draw mode
   useEffect(() => {
@@ -87,24 +87,24 @@ export function GraphArea({setRequest}:any){
     drawMode ? edgeHandles.enableDrawMode() : edgeHandles.disableDrawMode();
   }, [drawMode, edgeHandles]);
 
-  function handleOpenSetupModal(){
+  function handleOpenSetupModal() {
     setIsSetupModal(true)
   }
 
-  function handleCloseSetupModal(){
+  function handleCloseSetupModal() {
     setIsSetupModal(false)
   }
-  
+
   function handleChangeZoomLevel(level: number) {
 
-    const newSpacing = cy?.zoom() +(level)
+    const newSpacing = cy?.zoom() + (level)
     // setSpacingFactorElements(newSpacing)
 
     cy?.zoom(newSpacing);
   }
 
-  function handleChangeSpacingFactor(spacing: number){
-    
+  function handleChangeSpacingFactor(spacing: number) {
+
     cy?.layout({
       name: "preset",
       spacingFactor: spacing,
@@ -112,7 +112,7 @@ export function GraphArea({setRequest}:any){
     }).run()
   }
 
-  function AddEle(){
+  function AddEle() {
     try {
       cy.center(
         cy.add({
@@ -130,69 +130,69 @@ export function GraphArea({setRequest}:any){
             "type": "t",
             "value": 0,
             "weight": 0
-            },
+          },
           position: { x: 1, y: 1 },
         })
       )
 
-   }
-   catch (e) {
+    }
+    catch (e) {
       console.log('error not AddEle');
-   }
+    }
   }
 
-  function DelEle(){
+  function DelEle() {
     try {
-      var ele = cy.$('#'+element.id);
-      cy.remove( ele );
+      var ele = cy.$('#' + element.id);
+      cy.remove(ele);
     }
     catch (e) {
       console.log('error');
     }
   }
 
-  function handleUploadRequestJSON(file:any){ 
+  function handleUploadRequestJSON(file: any) {
     const reader = new FileReader();
-    reader.onload = function(e: any) {
+    reader.onload = function (e: any) {
       // console.log(JSON.parse(e.target.result));
-      setRequest(JSON.parse(e.target.result)); 
+      setRequest(JSON.parse(e.target.result));
     };
     try {
       reader.readAsText(file.target.files[0]);
     } catch (error) {
-      console.log(error,'reader');
+      console.log(error, 'reader');
     }
   };
 
-  function handleClick (){
+  function handleClick() {
     hiddenFileRequestInput.current.click();
   };
 
   function handleChangeLayout() {
     cy?.layout(layouts[count]).run();
-    count = (count+1)%layouts.length
+    count = (count + 1) % layouts.length
   }
 
-  function handleEdgehandles(){
+  function handleEdgehandles() {
     setDrawMode(!drawMode)
   }
-   
 
-  return(
+
+  return (
     <Container>
       {/*------ uper options graph buttons --------- */}
       <NavOptions>
         <ul>
-          <li className="tooltip">   
-             <AiOutlineZoomIn fontSize="1.5em"  onClick={()=>handleChangeZoomLevel(+.1)} /> 
-             <AiOutlineZoomOut fontSize="1.5em" onClick={()=>handleChangeZoomLevel(-.1)} /> 
-          <span className="tooltiptext">Drag zoom in or zoom out</span> 
+          <li className="tooltip">
+            <AiOutlineZoomIn fontSize="1.5em" onClick={() => handleChangeZoomLevel(+.1)} />
+            <AiOutlineZoomOut fontSize="1.5em" onClick={() => handleChangeZoomLevel(-.1)} />
+            <span className="tooltiptext">Drag zoom in or zoom out</span>
           </li>
 
           <li className="tooltip">
-             <AiOutlineExpandAlt fontSize="1.5em" onClick={()=>{handleChangeSpacingFactor(1.1)}}/>   
-             <AiOutlineShrink fontSize="1.5em" onClick={()=>{handleChangeSpacingFactor(0.9)}}/>   
-            <span className="tooltiptext">Undo and Redo in elements Graph</span> 
+            <AiOutlineExpandAlt fontSize="1.5em" onClick={() => { handleChangeSpacingFactor(1.1) }} />
+            <AiOutlineShrink fontSize="1.5em" onClick={() => { handleChangeSpacingFactor(0.9) }} />
+            <span className="tooltiptext">Controll spacing between elements Graph</span>
           </li>
           {/* <li className="tooltip">
              <BiUndo fontSize="1.5em" onClick={()=>{handleChangeSpacingFactor(1.1)}}/>   
@@ -200,59 +200,59 @@ export function GraphArea({setRequest}:any){
             <span className="tooltiptext">Undo and Redo in elements Graph</span> 
           </li> */}
 
-          <li className="tooltip"> 
-             <HiOutlineViewGridAdd fontSize="1.5em" cursor="pointer" onClick={AddEle}/>
-             <RiChatDeleteLine fontSize="1.5em" cursor="pointer" onClick={DelEle} />  
-            <span className="tooltiptext">Add or remove element graph</span> 
+          <li className="tooltip">
+            <HiOutlineViewGridAdd fontSize="1.5em" cursor="pointer" onClick={AddEle} />
+            <RiChatDeleteLine fontSize="1.5em" cursor="pointer" onClick={DelEle} />
+            <span className="tooltiptext">Add or remove element graph</span>
           </li>
-          
-          <li className="tooltip"> 
+
+          <li className="tooltip">
             <input type="file" name="UploadJSON" id="UploadJSON" ref={hiddenFileRequestInput} onChange={handleUploadRequestJSON} />
-            <BiGitPullRequest fontSize="1.5em"  cursor="pointer"  onClick={handleClick} />
-            <span className="tooltiptext">Upload archive json from requests</span> 
+            <BiGitPullRequest fontSize="1.5em" cursor="pointer" onClick={handleClick} />
+            <span className="tooltiptext">Upload archive json from requests</span>
           </li>
 
-          <li className="tooltip">   
-            <BsGear fontSize="1.5em" cursor="pointer" onClick={handleOpenSetupModal}  /> 
-          <span className="tooltiptext">Open setup modal</span> 
-          </li>
-        
-          <li className="tooltip">   
-            <BsLayoutWtf fontSize="1.5em" cursor="pointer" onClick={handleChangeLayout}  /> 
-          <span className="tooltiptext">Change layout</span> 
-          </li>
-          
-          <li className="tooltip">  
-            <TbFocusCentered fontSize="1.5em" cursor="pointer" onClick={() => cy.center()} /> 
-          <span className="tooltiptext">Center</span> 
+          <li className="tooltip">
+            <BsGear fontSize="1.5em" cursor="pointer" onClick={handleOpenSetupModal} />
+            <span className="tooltiptext">Open setup modal</span>
           </li>
 
-          <li className="tooltip">  
-            <FiPlayCircle fontSize="1.5em" cursor="not-allowed" onClick={handleEdgehandles} /> 
-          <span className="tooltiptext">{drawMode ? 'Draw On' : 'Draw Off'}</span> 
+          <li className="tooltip">
+            <BsLayoutWtf fontSize="1.5em" cursor="pointer" onClick={handleChangeLayout} />
+            <span className="tooltiptext">Change layout</span>
           </li>
-        
-          <li className="tooltip">  
-            <FiPlayCircle color="#228f41" fontSize="1.5em" cursor="not-allowed" onClick={() => cy.center()} /> 
-          <span className="tooltiptext">Run ??</span> 
+
+          <li className="tooltip">
+            <TbFocusCentered fontSize="1.5em" cursor="pointer" onClick={() => cy.center()} />
+            <span className="tooltiptext">Center</span>
           </li>
-        
+
+          <li className="tooltip">
+            <FiPlayCircle fontSize="1.5em" cursor="not-allowed" onClick={handleEdgehandles} />
+            <span className="tooltiptext">{drawMode ? 'Draw On' : 'Draw Off'}</span>
+          </li>
+
+          <li className="tooltip">
+            <FiPlayCircle color="#228f41" fontSize="1.5em" cursor="not-allowed" onClick={() => cy.center()} />
+            <span className="tooltiptext">Run ??</span>
+          </li>
+
         </ul>
       </NavOptions>
-      
+
       {/*------ StartGraph (GraphContainer / UploadGraph) --------- */}
       <GraphContainer>
         <StartGraph />
       </GraphContainer>
-    
+
       {/*------ SetupModal --------- */}
       <Suspense fallback={<div>Loading...</div>}>
-        < SetupModal 
+        < SetupModal
           isOpen={isSetupModal}
           onRequestClose={handleCloseSetupModal}
-          />
+        />
       </Suspense>
-      
+
     </Container>
   )
 }
